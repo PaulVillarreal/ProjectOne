3a53fb98b2f1092bb4c026f7cc440db525537b52
Paul Villarreal <paul_jvillarreal@hotmail.com>
2017-06-24 21:36:49 -0500

    Added in animations for model

---
+++ ProjectOne/Assets/Scripts/Data/PlayerAnimations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations
+{
+    public const string StandingPose = "Female_Standing_Pose";
+    public const string BackFlip = "backflip";
+    public const string HitInBody = "big_body_blow";
+    public const string Climbing1 = "climbing";
+    public const string Climbing2 = "climbing_1";
+    public const string StandUp = "crouched_to_standing";
+    public const string Death1 = "falling_back_death";
+    public const string LandAndRoll = "falling_to_roll";
+    public const string Run1 = "fast_run";
+    public const string Hanging = "hanging_idle";
+    public const string JumpOver = "jump_over";
+    public const string JumpUp1 = "jumping";
+    public const string JumpUp2 = "jumping_1";
+    public const string JumpDown = "jumping_down";
+    public const string Run2 = "running_inPlace";
+    public const string Slide = "running_slide";
+    public const string Sitting = "sitting_idle";
+    public const string Fall = "soccer_tackle";
+    public const string StandUpFromGround = "standing";
+    public const string Death2 = "standing_death_forward_01";
+    public const string Death3 = "standing_death_right_01";
+    public const string JumpUp3 = "standing_jump";
+    public const string ActionFlip = "stylish_flip";
+    public const string Walking = "walking";
+    public const string WallRun = "wall_run";
+
+}
